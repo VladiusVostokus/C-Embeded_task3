4f55f36b5b424bc40b563a043c40821301b669c1
VladiusVostokus <kovalenkov222@gmail.com>
2024-09-15 11:27:42 +0300

    check on equlity 2 inseparated strings

--- main.c
+++ main.c
@@ -3,13 +3,30 @@
 
 char str1[30];
 char str2[30];
+char *ptr1 = str1;
+char *ptr2 = str2;
 
 int main() {
    printf("Enter string 1:");
    scanf("%s",str1);
-   printf("Enter string 1:");
+   printf("Enter string 2:");
    scanf("%s",str2);
-   printf("%s\n%s\n",str1, str2);
+
+   while(1) {
+      char char1 = *ptr1;
+      char char2 = *ptr2;
+      if (char1 != char2) {
+         printf("These 2 strings are not equal\n");
+         break;;
+      }
+      if (char1 == '\0' || char2 == '\0') {
+         printf("These 2 strings are equal");
+         break;
+      }
+      ptr1++;
+      ptr2++;
+   }
+   //printf("%s\n%s\n%c",str1, str2, str1);
    return 0;
 }
 
