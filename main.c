b4352e0419397f4d3581dea7588655416d5a7411
VladiusVostokus <kovalenkov222@gmail.com>
2024-09-15 11:14:55 +0300

    get 2 strings from input

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+char str1[30];
+char str2[30];
 
 int main() {
-   printf("Hello, World!\n");
+   printf("Enter string 1:");
+   scanf("%s",str1);
+   printf("Enter string 1:");
+   scanf("%s",str2);
+   printf("%s\n%s\n",str1, str2);
    return 0;
 }
+
